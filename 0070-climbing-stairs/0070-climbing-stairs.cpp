e3a38e3fc878811812b21a3b0caed9d5a3918c25
ShubhamAgarwal-11 <114098580+ShubhamAgarwal-11@users.noreply.github.com>
2022-12-12 22:58:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//     DP
     // int v[46];
     // int help(int n){
     //     if(n == 0)
@@ -18,16 +19,34 @@ public:
     //     return help(n);
     // }
     
+    
+//     Bottom up
+    // int climbStairs(int n) {
+    //     vector<int>v(n+1);
+    //     if(n==1 || n==2)
+    //         return n;
+    //     v[0]=0;
+    //     v[1]=1;
+    //     v[2]=2;
+    //     for(int i=3;i<=n;i++){
+    //         v[i]=v[i-1]+v[i-2];
+    //     }
+    //     return v[n];
+    // }
+    
+//     Fibonacci pattern
     int climbStairs(int n) {
-        vector<int>v(n+1);
         if(n==1 || n==2)
             return n;
-        v[0]=0;
-        v[1]=1;
-        v[2]=2;
+        int a=1;
+        int b=2;
+        int c=3;
         for(int i=3;i<=n;i++){
-            v[i]=v[i-1]+v[i-2];
+            c=a+b;
+            a=b;
+            b=c;
         }
-        return v[n];
+        return c;
     }
+    
 };
