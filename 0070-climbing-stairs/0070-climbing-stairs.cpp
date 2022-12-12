8612f7aa66bb9c76469d2526f012a8c69ca962f4
ShubhamAgarwal-11 <114098580+ShubhamAgarwal-11@users.noreply.github.com>
2022-12-12 22:51:36 +0530

    Time: 3 ms (34.80%), Space: 6.2 MB (29.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int v[46];
-    int help(int n){
-        if(n == 0)
-            return 1;
-        if(n<0)
-            return 0;
-        if(v[n]!=-1)
-            return v[n];
-        int ans1= help(n-1);
-        int ans2=help(n-2);
-        v[n]=ans1+ans2;
-        return v[n];
-    }
+    // int v[46];
+    // int help(int n){
+    //     if(n == 0)
+    //         return 1;
+    //     if(n<0)
+    //         return 0;
+    //     if(v[n]!=-1)
+    //         return v[n];
+    //     int ans1= help(n-1);
+    //     int ans2=help(n-2);
+    //     v[n]=ans1+ans2;
+    //     return v[n];
+    // }
+    // int climbStairs(int n) {
+    //     memset(v,-1,sizeof(v));
+    //     return help(n);
+    // }
+    
     int climbStairs(int n) {
-        memset(v,-1,sizeof(v));
-        return help(n);
+        vector<int>v(n+1);
+        if(n==1 || n==2)
+            return n;
+        v[0]=0;
+        v[1]=1;
+        v[2]=2;
+        for(int i=3;i<=n;i++){
+            v[i]=v[i-1]+v[i-2];
+        }
+        return v[n];
     }
 };
