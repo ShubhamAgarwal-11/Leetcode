695c32340fbbca84c0dc26a5ecceff31a2265df0
ShubhamAgarwal-11 <114098580+ShubhamAgarwal-11@users.noreply.github.com>
2022-12-17 23:32:22 +0530

    Time: 31 ms (30.71%), Space: 12.4 MB (13.00%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,31 +1,63 @@
+// class Solution {
+// public:
+//     long evaluate(long a,long b,string x){
+//         if(x == "+")
+//             return a+b;
+//         else if(x == "-")
+//             return a-b;
+//         else if(x == "*")
+//             return a*b;
+//         else if(x == "/")
+//             return a/b;
+//         return 0;
+//     }
+//     int evalRPN(vector<string>& tokens) {
+//         stack<long>element;
+//         for(auto &x : tokens){
+//             if(x == "+" || x == "-" || x == "*" || x == "/"){
+//                 long a=element.top();
+//                 element.pop();
+                
+//                 long b=element.top();
+//                 element.pop();
+                
+//                 long ans = evaluate(b,a,x);
+//                 element.push(ans);
+//             }else
+//                 element.push(stoi(x));
+//         }
+//         return element.top();
+//     }
+// };
+
+
+//     using UnorderedMap
 class Solution {
 public:
-    long evaluate(long a,long b,string x){
-        if(x == "+")
-            return a+b;
-        else if(x == "-")
-            return a-b;
-        else if(x == "*")
-            return a*b;
-        else if(x == "/")
-            return a/b;
-        return 0;
-    }
     int evalRPN(vector<string>& tokens) {
         stack<long>element;
-        for(auto &x : tokens){
-            if(x == "+" || x == "-" || x == "*" || x == "/"){
-                long a=element.top();
+        unordered_map<string,function<long(long,long)>>umap={
+            {"+",[](long a,long b){return a+b;}},
+            {"-",[](long a,long b){return a-b;}},
+            {"*",[](long a,long b){return a*b;}},
+            {"/",[](long a,long b){return a/b;}}
+        };
+         for(auto &x : tokens){
+           if(x == "+" || x == "-" || x == "*" || x == "/"){
+                long a =element.top();
                 element.pop();
                 
                 long b=element.top();
                 element.pop();
                 
-                long ans = evaluate(b,a,x);
+                long ans= umap[x](b,a);
                 element.push(ans);
             }else
-                element.push(stoi(x));
-        }
-        return element.top();
+               element.push(stoi(x));
+         }
+        return element.top();        
     }
 };
+
+
+
