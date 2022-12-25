f9b6a1a5cb7732e37d8977ccb2487e1b22ffda25
ShubhamAgarwal-11 <114098580+ShubhamAgarwal-11@users.noreply.github.com>
2022-12-25 16:00:59 +0530

    Time: 26 ms (82.20%), Space: 13.6 MB (83.30%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,19 +1,52 @@
+// class Solution {
+// public:
+    
+//     O(n*m)
+//     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+//         int size=queries.size();
+//         vector<int>ans;
+//         int j=0;
+//         sort(nums.begin(),nums.end());
+//         while(j<queries.size()){
+//             int sum=0,count=0,i=0;
+//              while((i+1<=nums.size()) && (sum+nums[i]<=queries[j])){
+//                 sum+=nums[i];
+//                 i++;
+//                 ++count;
+//             }
+//             j++;
+//             ans.push_back(count);
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
+    int BinarySearch(vector<int>&prefix,int target){
+        int lb=0,ub=prefix.size()-1;
+        int mid,result=-1;
+        while(lb<=ub){
+            mid=(lb+ub)/2;
+            if(prefix[mid]<=target){
+                result=mid;
+                lb=mid+1;
+            }else
+                ub=mid-1;
+        }
+        return result+1;
+    }
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
-        int size=queries.size();
         vector<int>ans;
-        int j=0;
         sort(nums.begin(),nums.end());
-        while(j<queries.size()){
-            int sum=0,count=0,i=0;
-             while((i+1<=nums.size()) && (sum+nums[i]<=queries[j])){
-                sum+=nums[i];
-                i++;
-                ++count;
-            }
-            j++;
-            ans.push_back(count);
+        for(int i=1;i<nums.size();i++){
+            nums[i]=nums[i]+nums[i-1];
+        }
+        
+        for(int i=0;i<queries.size();i++){
+            int smallAns=BinarySearch(nums,queries[i]);
+            ans.push_back(smallAns);
         }
         return ans;
     }
