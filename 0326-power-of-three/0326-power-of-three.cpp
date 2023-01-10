60df77f3b0b1d6809ae9bb2f0a9eb0313f2bc418
ShubhamAgarwal-11 <114098580+ShubhamAgarwal-11@users.noreply.github.com>
2023-01-10 16:18:37 +0530

    Time: 11 ms (98.46%), Space: 5.7 MB (92.96%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,11 +1,28 @@
+//           approach 1
+// class Solution {
+// public:
+//     bool solve(int num,long long ans){
+//         if(num < ans){ return false; }
+//         if(num == ans) { return true; }
+//         return solve(num,ans*3);
+//     }
+//     bool isPowerOfThree(int n) {
+//         return solve(n,1);
+//     }
+// };
+
+
+//           approach 2
 class Solution {
 public:
-    bool solve(int num,long long ans){
-        if(num < ans){ return false; }
-        if(num == ans) { return true; }
-        return solve(num,ans*3);
-    }
     bool isPowerOfThree(int n) {
-        return solve(n,1);
+        if(n<=0) { return false; }
+        if(n == 1) { return true; }
+       
+        while(n%3 == 0)
+            n=n/3;
+        if(n == 1)
+            return true;
+        return false;
     }
 };
