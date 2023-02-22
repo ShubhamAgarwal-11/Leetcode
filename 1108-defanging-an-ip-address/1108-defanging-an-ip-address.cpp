8b9f4d4de9d96486cd81b43e17cdebabad24945b
ShubhamAgarwal-11 <114098580+ShubhamAgarwal-11@users.noreply.github.com>
2023-02-23 00:00:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.88%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans="";
+        for(int i=0;i<address.size();i++){
+            if(address[i] != '.')
+                ans.push_back(address[i]);
+            else{
+                ans.push_back('[');
+                ans.push_back(address[i]);
+                ans.push_back(']');
+            }
+        }
+        return ans;
+    }
+};
