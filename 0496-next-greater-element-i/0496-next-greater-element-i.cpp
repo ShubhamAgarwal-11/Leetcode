335d01857eb83806423608afc2a0284a493293e6
ShubhamAgarwal-11 <114098580+ShubhamAgarwal-11@users.noreply.github.com>
2023-11-30 23:25:12 +0530

    Time: 14 ms (20.11%), Space: 8.9 MB (85.37%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -9,28 +9,24 @@ public:
     }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int>temp;
-        vector<int>result;
         stack<int>st;
-        
         for(int i=nums2.size()-1;i>=0;i--){
-            if(st.empty()){
+            if(st.empty())
                 temp.push_back(-1);
-            }else if(st.size()>0 && st.top()>nums2[i]){
-                // cout<<"Sun le";
+            else if(!st.empty() && st.top() > nums2[i])
                 temp.push_back(st.top());
-            }else if(st.size()>0 && st.top()<=nums2[i]){
-                while(st.size()>0 && st.top()<nums2[i]){
+            else if(!st.empty() && st.top()<= nums2[i]){
+                while(!st.empty() && st.top() < nums2[i])
                     st.pop();
-                }
-                if(st.size()<=0){
+                if(st.empty())
                     temp.push_back(-1);
-                }else{
+                else
                     temp.push_back(st.top());
-                }
             }
             st.push(nums2[i]);
         }
         reverse(temp.begin(),temp.end());
+        vector<int>result;
         for(int i=0;i<nums1.size();i++){
             int index = search(nums2,nums1[i]);
             result.push_back(temp[index]);
