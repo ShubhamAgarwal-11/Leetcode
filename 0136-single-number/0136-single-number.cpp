a9adb9ab53e2daca3cd1bd10d7e9a9918b42a887
ShubhamAgarwal-11 <114098580+ShubhamAgarwal-11@users.noreply.github.com>
2023-12-19 22:54:07 +0530

    Time: 15 ms (63.73%), Space: 17.2 MB (81.22%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,27 @@
-class Solution {
+class Solution {   //  Time:- O(n)  Space:- O(1)
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i;
-        for(i=0;i<nums.size()-1;i++){
-            if(nums[i] != nums[i+1])
-                return nums[i];
-            else
-                i++;
-        }
-        return nums[i];
+        int result=0;
+        
+        for(int i=0;i<nums.size();i++)
+            result^=nums[i];
+        
+        return result;
     }
 };
+
+//                      Time:- O(nlogn)   Space:- O(1)
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         int i;
+//         for(i=0;i<nums.size()-1;i++){
+//             if(nums[i] != nums[i+1])
+//                 return nums[i];
+//             else
+//                 i++;
+//         }
+//         return nums[i];
+//     }
+// };
