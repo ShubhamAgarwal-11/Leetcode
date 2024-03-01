e6501268e4043612fa3bac0ff7e5785712f7cd06
ShubhamAgarwal-11 <114098580+ShubhamAgarwal-11@users.noreply.github.com>
2024-03-02 00:44:14 +0530

    Time: 4 ms (59.10%), Space: 12.5 MB (42.24%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    
-//     using Run length encoding algo.
     int compress(vector<char>& chars) {
-        int n= chars.size();
-        int i=0;
-        int index=0;
-        while(i<n){
-            int curr_char = chars[i];
-            int count=0;
-            
-            while(i<n && chars[i] == curr_char){
-                i++;
-                count++;
+        int index =0,i=0;
+        int size = chars.size();
+        while(i<size){
+            char ele = chars[i];
+            int count =0;
+            while(i<size && chars[i] == ele){
+                count++; i++;
             }
             
-            chars[index]=curr_char;
+            chars[index] = ele;
             index++;
-            
-            if(count>1){
-                string char_counts = to_string(count);
-                for(auto&x : char_counts){
-                    chars[index] = x;
-                    index++;
-                }   
+            if(count > 1){
+                for(auto num : to_string(count)){
+                    chars[index] = num;
+                    index++;                    
+                }
             }
-            
         }
+        
         return index;
     }
 };
